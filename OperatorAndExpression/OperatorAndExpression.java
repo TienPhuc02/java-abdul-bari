4dea302f92efd96381297501ad160a8cf6328fd1
TienPhuc02 <dtp2352002@gmail.com>
2024-01-30 17:19:58 +0700

    arthimetic operator

---
+++ OperatorAndExpression/OperatorAndExpression.java
@@ -0,0 +1,36 @@
+package OperatorAndExpression;
+
+public class OperatorAndExpression {
+    public static void main(String[] args) {
+        //arithmetic
+        // int a=14,b=5;
+        //  float a=14.3f,b=5.2f;
+        // // int c=a/b;
+        // float c=(float)a/b;
+        // // int r=a%b;
+        // float r=a%b;
+        // System.out.println(c);
+        // System.out.println(r);
+
+
+        // byte a=10;
+        // short b=15;
+        // int c=a+b; // error type with byte short -> int 
+        // float  a=12.5f;
+        // // long b=123l;
+        // // int b=123;
+        // double b=123;
+        // double c=a*b;
+
+        
+        // char a=40;
+        // int b=30;
+        // int c=a-b;
+        System.out.println(10/2*5);  
+    }
+}
+//int + int
+//float + float 
+// double +double
+// MOD : là phép chia lấy dư
+//arithametic : +, -, * , /
