3f75818cdd8d24c982b112af4f6ce35c21e771ae
TienPhuc02 <dtp2352002@gmail.com>
2024-01-31 08:48:38 +0700

    student challenge part2

--- OperatorAndExpression/StudentChallenge.java
+++ OperatorAndExpression/StudentChallenge.java
@@ -14,17 +14,25 @@ public class StudentChallenge {
 
 
 
-        float canh1,canh2,canh3,area1;
-        double areaMain;
-        canh1=sc.nextFloat();
-        canh2=sc.nextFloat();
-        canh3=sc.nextFloat();
-        area1=(canh1+canh2+canh3)/2;
-        areaMain=Math.sqrt(area1*(area1-canh1)*(area1-canh2)*(area1-canh3));
-        System.out.println(areaMain);
+        // float canh1,canh2,canh3,area1;
+        // double areaMain;
+        // canh1=sc.nextFloat();
+        // canh2=sc.nextFloat();
+        // canh3=sc.nextFloat();
+        // area1=(canh1+canh2+canh3)/2;
+        // areaMain=Math.sqrt(area1*(area1-canh1)*(area1-canh2)*(area1-canh3));
+        // System.out.println(areaMain);
 
 
+        int a,b,c;
+        double r1,r2;
 
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
 
+        r1=((-b+Math.sqrt(b*b-4*a*c))/(2*a));
+        r2=((-b-Math.sqrt(b*b-4*a*c))/(2*a));
+        System.out.println(r1 +" " + r2);
     }
 }
